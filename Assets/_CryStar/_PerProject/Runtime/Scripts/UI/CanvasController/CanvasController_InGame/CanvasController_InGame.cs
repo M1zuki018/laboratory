4766a27b99c923a2d0b10838e9b4846d800269da
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-23 22:48:33 +0900

    add:現在時刻のUIを追加

--- Assets/_CryStar/_PerProject/Runtime/Scripts/UI/CanvasController/CanvasController_InGame/CanvasController_InGame.cs
+++ Assets/_CryStar/_PerProject/Runtime/Scripts/UI/CanvasController/CanvasController_InGame/CanvasController_InGame.cs
@@ -1,7 +1,11 @@
 using System;
 using CryStar.Attribute;
+using CryStar.Core;
+using CryStar.PerProject;
+using CryStar.Utility;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace iCON.UI
 {
@@ -11,6 +15,12 @@ namespace iCON.UI
     public partial class CanvasController_InGame : WindowBase
     {
         [SerializeField, HighlightIfNull] private CustomButton _button;
+        [SerializeField] private Text _date;
+        
+        /// <summary>
+        /// 時間管理クラス
+        /// </summary>
+        private TimeController _timeController;
         
         public event Action OnButtonClicked;
                 
@@ -21,14 +31,45 @@ namespace iCON.UI
             
             return base.OnAwake();
         }
+
+        public override async UniTask OnBind()
+        {
+            await base.OnBind();
+            _timeController = ServiceLocator.GetLocal<TimeController>();
+            if (_timeController == null)
+            {
+                // 正常に取得できなかった場合はエラーログを出したうえで
+                // テキストが表示されないようにする
+                LogUtility.Error($"[{nameof(CanvasController_InGame)}]{nameof(_timeController)} が取得できませんでした");
+                _date.enabled = false;
+                return;
+            }
+
+            // 取得できている場合のみバインドを行い、テキストを初期化
+            _timeController.OnTimeChanged += UpdateDateText;
+            _date.text = _timeController.GetTimeText;
+        }
+        
         
         private void Temporary()
         {
+        }
+
+        /// <summary>
+        /// 日時の表記を更新する
+        /// </summary>
+        private void UpdateDateText()
+        {
+            if (_date != null && _timeController != null)
+            {
+                _date.text = _timeController.GetTimeText;
+            }
         }
         
         private void OnDestroy()
         {
             if(_button != null) _button.onClick?.RemoveAllListeners();
+            if(_timeController != null) _timeController.OnTimeChanged -= UpdateDateText;
         }
     }
 }
