a0481a02a4c31961cdc2c195358917844f7f08f9
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-24 22:47:58 +0900

    feat:キャラクターの種類の列挙型

---
+++ Assets/_CryStar/_PerProject/Runtime/Scripts/System/Location/CharacterType.cs
@@ -0,0 +1,34 @@
+namespace CryStar.PerProject
+{
+    /// <summary>
+    /// キャラクター名の列挙側
+    /// 1オリジン。先頭にNoneが定義されている
+    /// </summary>
+    public enum CharacterType
+    {
+        /// <summary>
+        /// データなし
+        /// </summary>
+        None,
+        
+        /// <summary>
+        /// カリル・ラセール
+        /// </summary>
+        Khalil,
+        
+        /// <summary>
+        /// フィルウ・ブランシャール
+        /// </summary>
+        Filou,
+        
+        /// <summary>
+        /// イーシャ・バラット
+        /// </summary>
+        Isha,
+        
+        /// <summary>
+        /// ユール・ウォード
+        /// </summary>
+        Yule,
+    }
+}
