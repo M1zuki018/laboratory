7ad965be24c337c688da792fbbaf78982b32d307
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-24 22:48:13 +0900

    feat:キャラクターが配置される場所の列挙型

---
+++ Assets/_CryStar/_PerProject/Runtime/Scripts/System/Location/LocationType.cs
@@ -0,0 +1,43 @@
+namespace CryStar.PerProject
+{
+    /// <summary>
+    /// キャラクターがいる場所の列挙型
+    /// </summary>
+    public enum LocationType
+    {
+        /// <summary>
+        /// 研究室西　奥
+        /// </summary>
+        WestLab_Rear,
+        
+        /// <summary>
+        /// 研究室西　中央
+        /// </summary>
+        WestLab_Center,
+        
+        /// <summary>
+        /// 研究室西　手前
+        /// </summary>
+        WestLab_Front,
+    
+        /// <summary>
+        /// 研究室東　奥右
+        /// </summary>
+        EastLab_RearRight,
+        
+        /// <summary>
+        /// 研究室東　奥左
+        /// </summary>
+        EastLab_RearLeft,
+        
+        /// <summary>
+        /// 研究室東　扉前
+        /// </summary>
+        EastLab_NearDoor,
+        
+        /// <summary>
+        /// 研究室東　手前
+        /// </summary>
+        EastLab_Front,
+    }
+}
