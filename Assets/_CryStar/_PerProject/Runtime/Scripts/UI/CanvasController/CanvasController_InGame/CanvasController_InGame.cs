bedcdf329fd2c6e64111d063504e0145bb2a3137
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-24 16:23:41 +0900

    fix:エリア変更に合わせた処理ができるように準備

--- Assets/_CryStar/_PerProject/Runtime/Scripts/UI/CanvasController/CanvasController_InGame/CanvasController_InGame.cs
+++ Assets/_CryStar/_PerProject/Runtime/Scripts/UI/CanvasController/CanvasController_InGame/CanvasController_InGame.cs
@@ -4,6 +4,7 @@ using CryStar.Utility;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
+using NotImplementedException = System.NotImplementedException;
 
 namespace iCON.UI
 {
@@ -13,15 +14,29 @@ namespace iCON.UI
     public class CanvasController_InGame : WindowBase
     {
         [SerializeField] private Text _date;
+        [SerializeField] private CustomImage _background;
+
+        [Header("デバッグ用")] 
+        [SerializeField] private Text _areaText;
         
-        /// <summary>
-        /// 時間管理クラス
-        /// </summary>
-        private TimeManager _timeManager;
+        private TimeManager _timeManager; // 時間管理
+        private AreaManager _areaManager; // 場所の管理
 
         public override async UniTask OnBind()
         {
             await base.OnBind();
+            
+            InitializeTimeView();
+            await InitializeBackground();
+        }
+        
+        #region 初期化メソッド
+
+        /// <summary>
+        /// 時間関連のViewの初期化を行う
+        /// </summary>
+        private void InitializeTimeView()
+        {
             _timeManager = ServiceLocator.GetLocal<TimeManager>();
             if (_timeManager == null)
             {
@@ -38,6 +53,39 @@ namespace iCON.UI
         }
         
         /// <summary>
+        /// 背景の初期化を行う
+        /// </summary>
+        private async UniTask InitializeBackground()
+        {
+            _areaManager = ServiceLocator.GetLocal<AreaManager>();
+            if (_areaManager == null)
+            {
+                // 正常に取得できなかった場合はエラーログを出したうえで
+                // テキストが表示されないようにする
+                LogUtility.Error($"[{nameof(CanvasController_InGame)}]{nameof(_areaManager)} が取得できませんでした");
+                return;
+            }
+            
+            // TODO: _backgroundの画像を適切なエリアの画像に差し替える処理
+            _areaText.text = _areaManager.CurrentArea.ToString(); // TODO: デバッグ用　後で消す
+            
+            // エリア移動時に背景素材を変更できるようにメソッドを登録
+            _areaManager.OnChangedArea += ChangeBackgroundSprite;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 移動したエリアに合わせて背景素材を変更する
+        /// </summary>
+        private void ChangeBackgroundSprite(AreaType areaType)
+        {
+            // TODO: 変更処理を作成
+            
+            _areaText.text = areaType.ToString(); // TODO: デバッグ用　後で消す
+        }
+        
+        /// <summary>
         /// 日時の表記を更新する
         /// </summary>
         private void UpdateDateText()
@@ -51,6 +99,7 @@ namespace iCON.UI
         private void OnDestroy()
         {
             if(_timeManager != null) _timeManager.OnTimeChanged -= UpdateDateText;
+            if(_areaManager != null) _areaManager.OnChangedArea -= ChangeBackgroundSprite;
         }
     }
 }
