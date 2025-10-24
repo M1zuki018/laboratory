9cdf45ea7ff091a1e4a1aee1723826e8e832f224
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-24 23:39:48 +0900

    add:エリア切り替えに対応

--- Assets/_CryStar/_PerProject/Runtime/Scripts/UI/CanvasController/CanvasController_InGame/UIContents_Characters.cs
+++ Assets/_CryStar/_PerProject/Runtime/Scripts/UI/CanvasController/CanvasController_InGame/UIContents_Characters.cs
@@ -14,37 +14,78 @@ namespace iCON.UI
     /// </summary>
     public class UIContents_Characters : CustomBehaviour
     {
+        [SerializeField] private CanvasGroup _eastLab;
+        [SerializeField] private CanvasGroup _westLab;
         [SerializeField] private List<ViewData> _characterButtonDataList = new List<ViewData>();
         
         [Header("Debug")]
         [SerializeField] private List<Sprite> _characterSpriteList = new List<Sprite>();
         
-        private LocationManager _locationManager;
+        private CharacterLocationManager _characterLocationManager; // キャラクターの場所を管理するクラス
+        private AreaManager _areaManager; // 現在のプレイヤーの居場所を管理するクラス
         
         #region Life cycle
 
         public override async UniTask OnBind()
         {
             await base.OnBind();
-            
-            _locationManager = ServiceLocator.GetLocal<LocationManager>();
-            if (_locationManager == null)
-            {
-                LogUtility.Error($"[{typeof(UIContents_Characters)}] ローカルサービスから{typeof(LocationManager)}が取得できませんでした");
-                return;
-            }
-            
-            _locationManager.OnMoveCharacter += HandleMoveCharacter;
+            InitializeCharacterLocation();
+            InitializeArea();
         }
 
         private void OnDestroy()
         {
-            if (_locationManager != null)
+            if (_characterLocationManager != null)
             {
-                _locationManager.OnMoveCharacter -= HandleMoveCharacter;
+                _characterLocationManager.OnMoveCharacter -= HandleMoveCharacter;
+            }
+
+            if (_areaManager != null)
+            {
+                _areaManager.OnChangedArea -= HandleChangeArea;
             }
         }
 
+        #endregion
+        
+        #region 初期化
+        
+        /// <summary>
+        /// CharacterLocationManager関連の初期化
+        /// </summary>
+        private void InitializeCharacterLocation()
+        {
+            _characterLocationManager = ServiceLocator.GetLocal<CharacterLocationManager>();
+            if (_characterLocationManager == null)
+            {
+                LogUtility.Error($"[{typeof(UIContents_Characters)}] ローカルサービスから{typeof(CharacterLocationManager)}が取得できませんでした");
+                return;
+            }
+            
+            _characterLocationManager.OnMoveCharacter += HandleMoveCharacter;
+        }
+
+        /// <summary>
+        /// AreaManager関連の初期化
+        /// </summary>
+        private void InitializeArea()
+        {
+            _areaManager = ServiceLocator.GetLocal<AreaManager>();
+            if (_areaManager == null)
+            {
+                LogUtility.Error($"[{typeof(UIContents_Characters)}] ローカルサービスから{typeof(AreaManager)}が取得できませんでした");
+                
+                // 万が一取得できなかった場合は西側のキャンバスを有効化しておく
+                // TODO: 対応方法検討
+                EnableCanvas(_westLab, true);
+                EnableCanvas(_eastLab, false);
+                return;
+            }
+
+            _areaManager.OnChangedArea += HandleChangeArea;
+            HandleChangeArea(_areaManager.CurrentArea);
+        }
+        
         #endregion
         
         /// <summary>
@@ -69,6 +110,38 @@ namespace iCON.UI
             // TODO: 正しい実装に変える　targetData.Button.image.sprite
             targetData.Button.image.sprite = _characterSpriteList[(int)character];
             targetData.Button.image.enabled = true;
+        }
+        
+        /// <summary>
+        /// プレイヤーがエリア移動したときに利用するCanvasGroupを切り替える処理
+        /// </summary>
+        private void HandleChangeArea(AreaType targetArea)
+        {
+            switch (targetArea)
+            {
+                case AreaType.WestLab:
+                    EnableCanvas(_westLab, true);
+                    EnableCanvas(_eastLab, false);
+                    break;
+                case AreaType.EastLab:
+                    EnableCanvas(_westLab, false);
+                    EnableCanvas(_eastLab, true);
+                    break;
+                default:
+                    EnableCanvas(_westLab, false);
+                    EnableCanvas(_eastLab, false);
+                break;
+            }
+        }
+
+        /// <summary>
+        /// キャンバスグループの有効/無効を切り替える
+        /// </summary>
+        private void EnableCanvas(CanvasGroup canvasGroup, bool enable)
+        {
+            canvasGroup.alpha = enable ? 1 : 0;
+            canvasGroup.interactable = enable;
+            canvasGroup.blocksRaycasts = enable;
         }
     }
 
