f5e2b30cecda2ba634af223d5d6a3b858c6aa1e5
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-26 09:10:35 +0900

    feat:時間帯の列挙型

---
+++ Assets/_CryStar/_PerProject/Runtime/Scripts/System/Time/TimeZoneType.cs
@@ -0,0 +1,28 @@
+namespace CryStar.PerProject
+{
+    /// <summary>
+    /// 時間帯の列挙型
+    /// </summary>
+    public enum TimeZoneType
+    {
+        /// <summary>
+        /// 朝
+        /// </summary>
+        Morning,
+        
+        /// <summary>
+        /// 昼
+        /// </summary>
+        Afternoon,
+        
+        /// <summary>
+        /// 夕方
+        /// </summary>
+        Evening,
+        
+        /// <summary>
+        /// 夜
+        /// </summary>
+        Night,
+    }
+}
