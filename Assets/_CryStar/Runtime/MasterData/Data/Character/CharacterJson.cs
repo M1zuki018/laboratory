70499869b8be2d017212ee250038b4b8a90606f1
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-27 00:14:30 +0900

    feat:Json用クラス

---
+++ Assets/_CryStar/Runtime/MasterData/Data/Character/CharacterJson.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// MasterCharacter
+/// </summary>
+[Serializable]
+public class CharacterJson
+{
+    public string version;
+    public List<CharacterInfo> characters;
+}
