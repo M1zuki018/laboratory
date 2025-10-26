3de4ec021ba770bc31ebe7fce21d7f1a3d8bee65
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-27 00:14:39 +0900

    feat:表情差分用クラス

---
+++ Assets/_CryStar/Runtime/MasterData/Data/Character/FacialExpressionVariants.cs
@@ -0,0 +1,11 @@
+using System;
+
+/// <summary>
+/// 表情差分のパス管理用のデータクラス
+/// </summary>
+[Serializable]
+public class FacialExpressionVariants
+{
+    /// <summary>デフォルト</summary>
+    public string defaultFace;
+}
