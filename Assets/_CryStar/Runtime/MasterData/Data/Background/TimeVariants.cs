3f357a9d48c130542e27d4a4c77127462ffee7af
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-26 23:07:18 +0900

    feat:時間帯ごとの背景アセットのパスを指定するためのデータクラス

---
+++ Assets/_CryStar/Runtime/MasterData/Data/Background/TimeVariants.cs
@@ -0,0 +1,13 @@
+using System;
+
+/// <summary>
+/// 時間帯ごとの背景差分用のJSON用データクラス
+/// </summary>
+[Serializable]
+public class TimeVariants
+{
+    public string morning;
+    public string afternoon;
+    public string evening;
+    public string night;
+}
