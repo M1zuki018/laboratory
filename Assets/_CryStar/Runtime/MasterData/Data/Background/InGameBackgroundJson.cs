24646edecf05824ba5fa21b2eed88db310b8e422
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-26 23:06:59 +0900

    feat:背景マスタのJSONの保存形式のデータクラス

---
+++ Assets/_CryStar/Runtime/MasterData/Data/Background/InGameBackgroundJson.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// MasterInGameBackground
+/// </summary>
+[Serializable]
+public class InGameBackgroundJson
+{
+    public string version;
+    public List<InGameBackgroundInfo> backgrounds;
+}
