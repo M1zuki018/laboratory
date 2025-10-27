966402dd434de9b39523dce811c6281641ff0275
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-27 14:49:44 +0900

    add:MasterAreaTalkの事前ロードを追加

--- Assets/_CryStar/_PerProject/Runtime/Scripts/System/SceneManager/InGameManager.cs
+++ Assets/_CryStar/_PerProject/Runtime/Scripts/System/SceneManager/InGameManager.cs
@@ -46,7 +46,7 @@ namespace iCON.System
         public override async UniTask OnStart()
         {
             await base.OnStart();
-            await MasterDataManager.Instance.PreloadAsync(typeof(MasterAreaData), typeof(MasterCharacter));
+            await MasterDataManager.Instance.PreloadAsync(typeof(MasterAreaData), typeof(MasterCharacter), typeof(MasterAreaTalk));
             
             // ストーリー再生時以外はゲームオブジェクトを非アクティブにしておく
             _storyOrchestrator.gameObject.SetActive(false);
