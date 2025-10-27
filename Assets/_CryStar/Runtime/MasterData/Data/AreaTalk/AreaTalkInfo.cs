46b29d522be67ccde0838909eede69941c93ebde
M1zuki018 <167507946+M1zuki018@users.noreply.github.com>
2025-10-27 14:45:34 +0900

    feat:Jsonで読み込むためのデータクラス

---
+++ Assets/_CryStar/Runtime/MasterData/Data/AreaTalk/AreaTalkInfo.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 会話データ
+/// </summary>
+[Serializable]
+public class AreaTalkInfo
+{
+    public List<AreaTalkEntry> talkDatas;
+}
